f9250f403d8919ee979015cbd89bfa60c4a5a70f
radunasor <radmehr1385@gmail.com>
2023-11-14 23:47:29 +0100

    impelemented insert_back/front

--- src/data_structures/list/list.c
+++ src/data_structures/list/list.c
@@ -1,28 +1,32 @@
+#include <string.h>
+
 #include "list.h"
 
 typedef struct _lc_list_node_t lc_list_node_t;
 
-struct
+struct _lc_list_node_t
 {
     void *data;
     lc_list_node_t *next;
     lc_list_node_t *prev;
-} _lc_list_node_t;
+};
 
-struct
+struct _lc_list_t
 {
     lc_list_node_t *head;
     lc_list_node_t *tail;
 
     lc_list_node_t *current_node;
     size_t list_size;
-} _lc_list_t;
+};
 
 lc_list_t *lc_list_create()
 {
-    lc_list_t *list = _malloc(sizeof(_lc_list_t));
+    lc_list_t *list = _malloc(sizeof(lc_list_t));
 
-    LC_ASSERT(list, "List create memory allocation returned NULL.");
+    LC_ASSERT(list, "List memory allocation returned NULL.");
+
+    memset(list, 0, sizeof(list));
 
     return list;
 }
@@ -34,14 +38,59 @@ void lc_list_destroy(lc_list_t *list)
 
 void lc_list_insert_front(lc_list_t *list, void *data)
 {
+    lc_list_node_t *new_node = _malloc(sizeof(lc_list_node_t));
+
+    LC_ASSERT(new_node, "List Node memory allocation returned NULL.");
+
+    memset(new_node, 0, sizeof(new_node));
+
+    new_node->data = data;
+
+    if (list->head)
+    {
+        new_node->next = list->head;
+        list->head->prev = new_node;
+        list->head = new_node;
+    }
+    else
+    {
+        list->head = new_node;
+        list->tail = new_node;
+    }
 }
 
 void lc_list_insert_back(lc_list_t *list, void *data)
 {
+    lc_list_node_t *new_node = _malloc(sizeof(lc_list_node_t));
+
+    LC_ASSERT(new_node, "List Node memory allocation returned NULL.");
+
+    memset(new_node, 0, sizeof(new_node));
+
+    new_node->data = data;
+
+    if (list->tail)
+    {
+        new_node->prev = list->tail;
+        list->tail->next = new_node;
+        list->tail = new_node;
+    }
+    else
+    {
+        list->head = new_node;
+        list->tail = new_node;
+    }
 }
 
 void lc_list_insert_at(lc_list_t *list, size_t index, void *data)
 {
+    lc_list_node_t *new_node = _malloc(sizeof(lc_list_node_t));
+
+    LC_ASSERT(new_node, "List Node memory allocation returned NULL.");
+
+    memset(new_node, 0, sizeof(new_node));
+
+    new_node->data = data;
 }
 
 void *lc_list_pop_front(lc_list_t *list)
