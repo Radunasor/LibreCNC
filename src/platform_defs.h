9c5e76dc90a01b97b78d60f3511e14a8c8cc99fd
radunasor <radmehr1385@gmail.com>
2023-11-14 23:47:49 +0100

    chnaged printf with log

--- src/platform_defs.h
+++ src/platform_defs.h
@@ -1,11 +1,12 @@
 #ifndef PLATFORM_DEFS_H
 #define PLATFORM_DEFS_H
 
-#include "librecnc_config.h"
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "librecnc_config.h"
+#include "logger/logger.h"
 
 #if defined(__STDC__) && __STDC_VERSION__ >= 199901L
 #include <stdbool.h>
@@ -32,14 +33,14 @@ typedef enum
         ;
 
 // todo : need to change printf with LOG_CRITICAL
-#define LC_ASSERT(expr, msg)                                                                         \
-    do                                                                                               \
-    {                                                                                                \
-        if (!(expr))                                                                                 \
-        {                                                                                            \
-            printf("Asserted: %s (%s, %s, %s, %d)\n", msg, #expr, __FILE__, __FUNCTION__, __LINE__); \
-            LC_ASSERT_HANDLER                                                                        \
-        }                                                                                            \
+#define LC_ASSERT(expr, msg)                                                                               \
+    do                                                                                                     \
+    {                                                                                                      \
+        if (!(expr))                                                                                       \
+        {                                                                                                  \
+            LC_LOG_ERROR("Asserted: %s (%s, %s, %s, %d)\n", msg, #expr, __FILE__, __FUNCTION__, __LINE__); \
+            LC_ASSERT_HANDLER                                                                              \
+        }                                                                                                  \
     } while (0)
 
 #define LC_DEBUG_BUILD
