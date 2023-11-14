d5b1e0e8a85710617a3eeb969a3728d437859a90
radunasor <radmehr1385@gmail.com>
2023-11-14 19:11:00 +0100

    list biolerplate added

---
+++ src/data_structures/list/list.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "platform_defs.h"
+
+typedef struct _tp_list_t tp_list_t;
+
+typedef void (*lc_list_iterate_cb_t)(void *data);
+
+tp_list_t *lc_list_create();
+void lc_list_destroy(tp_list_t *list);
+
+void lc_list_insert_front(tp_list_t *list, void *data);
+void lc_list_insert_back(tp_list_t *list, void *data);
+void lc_list_insert_at(tp_list_t *list, size_t index, void *data);
+
+void *lc_list_pop_front(tp_list_t *list);
+void *lc_list_pop_back(tp_list_t *list);
+void *lc_list_pop_at(tp_list_t *list, size_t index);
+
+void lc_list_clear(tp_list_t *list);
+
+void lc_list_iterate(tp_list_t *list, lc_list_iterate_cb_t iter_cb);
+
+bool lc_list_find(tp_list_t *list, void *data);
+
+size_t lc_list_get_size(tp_list_t *list);
+#endif
