be0116e14fc03fda3dcf66fe6ff5ae6d3a120b33
radunasor <radmehr1385@gmail.com>
2023-11-14 23:02:03 +0100

    minor changes

--- src/data_structures/list/list.c
+++ src/data_structures/list/list.c
@@ -18,50 +18,60 @@ struct
     size_t list_size;
 } _lc_list_t;
 
-tp_list_t *lc_list_create()
+lc_list_t *lc_list_create()
+{
+    lc_list_t *list = _malloc(sizeof(_lc_list_t));
+
+    LC_ASSERT(list, "List create memory allocation returned NULL.");
+
+    return list;
+}
+
+void lc_list_destroy(lc_list_t *list)
+{
+    _free(list);
+}
+
+void lc_list_insert_front(lc_list_t *list, void *data)
 {
 }
 
-void lc_list_destroy(tp_list_t *list)
+void lc_list_insert_back(lc_list_t *list, void *data)
 {
 }
 
-void lc_list_insert_front(tp_list_t *list, void *data)
+void lc_list_insert_at(lc_list_t *list, size_t index, void *data)
 {
 }
 
-void lc_list_insert_back(tp_list_t *list, void *data)
+void *lc_list_pop_front(lc_list_t *list)
 {
 }
 
-void lc_list_insert_at(tp_list_t *list, size_t index, void *data)
+void *lc_list_pop_back(lc_list_t *list)
 {
 }
 
-void *lc_list_pop_front(tp_list_t *list)
+void *lc_list_pop_at(lc_list_t *list, size_t index)
 {
 }
 
-void *lc_list_pop_back(tp_list_t *list)
+void lc_list_clear(lc_list_t *list)
 {
 }
 
-void *lc_list_pop_at(tp_list_t *list, size_t index)
+void *lc_list_find_at(lc_list_t *list, size_t index)
 {
 }
 
-void lc_list_clear(tp_list_t *list)
+void lc_list_foreach(lc_list_t *list, lc_list_iterate_cb_t iter_cb)
 {
 }
 
-void lc_list_iterate(tp_list_t *list, lc_list_iterate_cb_t iter_cb)
+bool lc_list_find(lc_list_t *list, void *data)
 {
 }
 
-bool lc_list_find(tp_list_t *list, void *data)
-{
-}
-
-size_t lc_list_get_size(tp_list_t *list)
+size_t lc_list_get_size(lc_list_t *list)
 {
 }
--- src/data_structures/list/list.h
+++ src/data_structures/list/list.h
@@ -3,26 +3,29 @@
 
 #include "platform_defs.h"
 
-typedef struct _tp_list_t tp_list_t;
+#define LIST_LOOP(list, node_data) for (size_t i = 0; i < lc_list_get_size(list); node_data = lc_list_find_at(list, i++))
+
+typedef struct _lc_list_t lc_list_t;
 
 typedef void (*lc_list_iterate_cb_t)(void *data);
 
-tp_list_t *lc_list_create();
-void lc_list_destroy(tp_list_t *list);
+lc_list_t *lc_list_create();
+void lc_list_destroy(lc_list_t *list);
 
-void lc_list_insert_front(tp_list_t *list, void *data);
-void lc_list_insert_back(tp_list_t *list, void *data);
-void lc_list_insert_at(tp_list_t *list, size_t index, void *data);
+void lc_list_insert_front(lc_list_t *list, void *data);
+void lc_list_insert_back(lc_list_t *list, void *data);
+void lc_list_insert_at(lc_list_t *list, size_t index, void *data);
 
-void *lc_list_pop_front(tp_list_t *list);
-void *lc_list_pop_back(tp_list_t *list);
-void *lc_list_pop_at(tp_list_t *list, size_t index);
+void *lc_list_pop_front(lc_list_t *list);
+void *lc_list_pop_back(lc_list_t *list);
+void *lc_list_pop_at(lc_list_t *list, size_t index);
 
-void lc_list_clear(tp_list_t *list);
+void lc_list_clear(lc_list_t *list);
 
-void lc_list_iterate(tp_list_t *list, lc_list_iterate_cb_t iter_cb);
+void *lc_list_find_at(lc_list_t *list, size_t index);
+void lc_list_foreach(lc_list_t *list, lc_list_iterate_cb_t iter_cb);
 
-bool lc_list_find(tp_list_t *list, void *data);
+bool lc_list_find(lc_list_t *list, void *data);
 
-size_t lc_list_get_size(tp_list_t *list);
+size_t lc_list_get_size(lc_list_t *list);
 #endif
