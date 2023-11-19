b12a793befa5809ed86ed4cd45f729cdd1da4d87
radunasor <radmehr1385@gmail.com>
2023-11-19 19:43:31 +0100

    new aproach to handle pops

--- src/data_structures/list/list.c
+++ src/data_structures/list/list.c
@@ -41,6 +41,48 @@ static lc_list_node_t *lc_list_get_node(lc_list_t *list, size_t index)
     return node_ptr;
 }
 
+static void *lc_list_node_erase(lc_list_t *list, lc_list_node_t *node_to_erase)
+{
+    void *data = NULL;
+
+    list->list_size -= 1;
+
+    if (node_to_erase == list->head)
+    {
+        lc_list_node_t *ex_head = list->head;
+
+        if (list->list_size > 1)
+            ex_head->next->prev = NULL;
+
+        list->head = ex_head->next;
+        data = ex_head->data;
+
+        _free(ex_head);
+
+        return data;
+    }
+
+    if (node_to_erase == list->tail)
+    {
+        lc_list_node_t *ex_tail = list->tail;
+
+        ex_tail->prev->next = NULL;
+        list->tail = ex_tail->prev;
+        data = ex_tail->data;
+
+        _free(ex_tail);
+        return data;
+    }
+
+    node_to_erase->prev->next = node_to_erase->next;
+    node_to_erase->next->prev = node_to_erase->prev;
+    data = node_to_erase->data;
+
+    _free(node_to_erase);
+
+    return data;
+}
+
 lc_list_t *lc_list_create()
 {
     lc_list_t *list = _malloc(sizeof(lc_list_t));
@@ -139,96 +181,21 @@ void *lc_list_pop_front(lc_list_t *list)
 {
     LC_ASSERT(list->list_size, "list pop front invalid size");
 
-    void *data = NULL;
-
-    if (list->list_size == 0)
-        return data;
-
-    if (list->list_size == 1)
-    {
-        data = list->head->data;
-
-        _free(list->head);
-
-        list->head = NULL;
-        list->tail = NULL;
-
-        list->list_size = 0;
-
-        return data;
-    }
-
-    lc_list_node_t *ex_head = list->head;
-    data = ex_head->data;
-    list->head = list->head->next;
-    list->head->prev = NULL;
-
-    _free(ex_head);
-
-    list->list_size -= 1;
-
-    return data;
+    return lc_list_node_erase(list, list->head);
 }
 
 void *lc_list_pop_back(lc_list_t *list)
 {
     LC_ASSERT(list->list_size, "list pop back invalid size");
 
-    void *data = NULL;
-
-    if (list->list_size == 0)
-        return data;
-
-    if (list->list_size == 1)
-    {
-        data = list->head->data;
-
-        _free(list->head);
-
-        list->head = NULL;
-        list->tail = NULL;
-
-        list->list_size = 0;
-
-        return data;
-    }
-
-    lc_list_node_t *ex_tail = list->tail;
-    data = ex_tail->data;
-    list->tail = list->tail->prev;
-    list->tail->next = NULL;
-
-    _free(ex_tail);
-
-    list->list_size -= 1;
-
-    return data;
+    return lc_list_node_erase(list, list->tail);
 }
 
 void *lc_list_pop_at(lc_list_t *list, size_t index)
 {
     LC_ASSERT(index < list->list_size, "pop index out of range");
 
-    void *data = NULL;
-
-    lc_list_node_t *target_node = lc_list_get_node(list, index);
-
-    if (!target_node)
-        return data;
-
-    data = target_node->data;
-
-    if (target_node != list->head)
-        target_node->prev->next = target_node->next;
-
-    if (target_node != list->tail)
-        target_node->next->prev = target_node->prev;
-
-    _free(target_node);
-
-    list->list_size -= 1;
-
-    return data;
+    return lc_list_node_erase(list, lc_list_get_node(list, index));
 }
 
 void lc_list_clear(lc_list_t *list)
