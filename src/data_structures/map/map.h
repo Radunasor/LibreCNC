070c9da3363f8d04afcbbb9c22c3a70fe929204e
radunasor <radmehr1385@gmail.com>
2023-11-20 21:26:18 +0100

    remove signature changed

--- src/data_structures/map/map.h
+++ src/data_structures/map/map.h
@@ -9,7 +9,7 @@ lc_map_t *lc_map_create();
 void lc_map_destroy(lc_map_t *map);
 
 void lc_map_insert(lc_map_t *map, void *key, size_t key_size, void *data, size_t data_size);
-void *lc_map_remove(lc_map_t *map, void *key);
+void *lc_map_remove(lc_map_t *map, void *key, size_t key_size);
 
 void *lc_map_find(lc_map_t *map, void *key, size_t key_size);
 
