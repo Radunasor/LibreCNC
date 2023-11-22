a02b05202ff929ba8b87f37a6c8b4b73fc38fcc0
radunasor <radmehr1385@gmail.com>
2023-11-22 17:48:39 +0100

    initilize config

--- src/librecnc.c
+++ src/librecnc.c
@@ -1,13 +1,30 @@
 #include "librecnc.h"
 
+#include "config/config.h"
+
+static bool initialized = false;
+
 void lc_init()
 {
+    if (initialized)
+        return;
+
+    lc_config_init();
+
+    initialized = true;
 }
 
 bool lc_get_initialized()
 {
+    return initialized;
 }
 
 void lc_deinit()
 {
+    if (!initialized)
+        return;
+
+    lc_config_deinit();
+
+    initialized = false;
 }
