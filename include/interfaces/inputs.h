8afc5c5687140a8b85ab482b6095cdbe7fc456b8
radunasor <radmehr1385@gmail.com>
2023-11-27 13:08:11 +0100

    file renaming

---
+++ include/interfaces/inputs.h
@@ -0,0 +1,20 @@
+#ifndef INPUTS_H
+#define INPUTS_H
+
+#include "platform_defs.h"
+
+typedef struct
+{
+    bool (*lc_interface_input_x_home_switch_trigged)();
+    bool (*lc_interface_input_x_end_switch_trigged)();
+
+    bool (*lc_interface_input_y_home_switch_trigged)();
+    bool (*lc_interface_input_y_end_switch_trigged)();
+
+    bool (*lc_interface_input_z_home_switch_trigged)();
+    bool (*lc_interface_input_z_end_switch_trigged)();
+
+    bool (*lc_interface_input_estop_switch_trigged)();
+} lc_interface_input_t;
+
+#endif
---
+++ include/interfaces/outputs.h
@@ -0,0 +1,13 @@
+#ifndef OUTPUTS_H
+#define OUTPUTS_H
+
+#include "platform_defs.h"
+
+typedef struct
+{
+    void (*lc_interface_output_x_axis_pulse)(bool state, bool dir);
+    void (*lc_interface_output_y_axis_pulse)(bool state, bool dir);
+    void (*lc_interface_output_z_axis_pulse)(bool state, bool dir);
+} lc_interface_output_t;
+
+#endif
