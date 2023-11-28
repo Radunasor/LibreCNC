1d19f2e9c71fa1c9d52a9a8a9636d24f098a0351
radunasor <radmehr1385@gmail.com>
2023-11-28 14:48:38 +0100

    ring buffer initial commite

---
+++ src/data_structures/ring_buffer/ring_buffer.h
@@ -0,0 +1,10 @@
+#include "common_defs.h"
+
+typedef struct _lc_ring_buffer_t lc_ring_buffer_t;
+
+lc_ring_buffer_t *lc_ring_buffer_create(size_t size);
+void lc_ring_buffer_destroy(lc_ring_buffer_t *rb);
+void lc_ring_buffer_insert(lc_ring_buffer_t *rb, void *data);
+void *lc_ring_buffer_remove(lc_ring_buffer_t *rb);
+size_t lc_ring_buffer_get_elemets_count(lc_ring_buffer_t *rb);
+size_t lc_ring_buffer_get_size(lc_ring_buffer_t *rb);
