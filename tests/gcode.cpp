d621924e71014e943bcb57624e0a7b683d7a93de
radunasor <radmehr1385@gmail.com>
2023-12-10 22:07:55 +0100

    add more human readable code

--- tests/gcode.cpp
+++ tests/gcode.cpp
@@ -19,7 +19,7 @@ bool lc_interface_gcode_deinit()
 static int ln_number = 0;
 bool lc_interface_gcode_get_line(char *line, size_t *line_number)
 {
-    const char *line_ptr = "G126.361 X5 Y6 Z7 T9 F1000 M123 \n";
+    const char *line_ptr = "G1 G5.1 X5 Y6 Z7 T9 F1000 M123 \n";
     *line_number = ln_number++;
     memcpy(line, line_ptr, strlen(line_ptr));
 
@@ -48,9 +48,12 @@ protected:
 TEST_F(LCGcode, initial_t)
 {
 
-    lc_gcode_set_callback(LC_GCODE_TYPE_G, [](uint16_t code, uint16_t subcode, lc_gcode_attrbute_value_t *values) {});
-    lc_gcode_set_callback(LC_GCODE_TYPE_F, [](uint16_t code, uint16_t subcode, lc_gcode_attrbute_value_t *values) {});
-    lc_gcode_set_callback(LC_GCODE_TYPE_M, [](uint16_t code, uint16_t subcode, lc_gcode_attrbute_value_t *values) {});
+    lc_gcode_set_callback(LC_GCODE_TYPE_G, [](uint16_t command, bool subcommand_existed, uint16_t sub_command, const lc_gcode_attrbute_value_t *values)
+                          { LC_LOG_INFO("Gcode command G parsed with Command %d%s", command, subcommand_existed ? (" and subcommand " + std::to_string(sub_command)).c_str() : "", sub_command) });
+    lc_gcode_set_callback(LC_GCODE_TYPE_F, [](uint16_t command, bool subcommand_existed, uint16_t sub_command, const lc_gcode_attrbute_value_t *values)
+                          { LC_LOG_INFO("Gcode command F parsed with Command %d%s", command, subcommand_existed ? (" and subcommand " + std::to_string(sub_command)).c_str() : "", sub_command) });
+    lc_gcode_set_callback(LC_GCODE_TYPE_M, [](uint16_t command, bool subcommand_existed, uint16_t sub_command, const lc_gcode_attrbute_value_t *values)
+                          { LC_LOG_INFO("Gcode command M parsed with Command %d%s", command, subcommand_existed ? (" and subcommand " + std::to_string(sub_command)).c_str() : "", sub_command) });
 
     lc_gcode_process_line();
 }
