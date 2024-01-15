8a9624a36e95ef2a18f59f843bc459a35410c679
radunasor <radmehr1385@gmail.com>
2024-01-15 13:03:04 +0100

    add new func to interface

--- include/interfaces/Igcode.h
+++ include/interfaces/Igcode.h
@@ -9,6 +9,7 @@ typedef struct
     bool (*lc_interface_gcode_deinit)();
 
     bool (*lc_interface_gcode_get_line)(char *line, size_t *line_number);
+    bool (*lc_interface_gcode_get_end_of_file)();
 } lc_interface_gcode_t;
 
 #endif
