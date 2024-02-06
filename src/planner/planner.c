0e3f556485d201f2f686fbc6cc6abd1be913b950
radunasor <radmehr1385@gmail.com>
2024-02-06 12:02:07 +0100

    change the gcode parser callback

--- src/planner/planner.c
+++ src/planner/planner.c
@@ -10,6 +10,8 @@ static bool initialized = false;
 /***********static functions declarations**************/
 /******************************************************/
 static void lc_planner_gcode_parser_callback(const lc_gcode_obj_t *parsed_gcode);
+static void lc_planner_on_G_command(const lc_gcode_obj_t *parsed_gcode);
+static void lc_planner_on_M_command(const lc_gcode_obj_t *parsed_gcode);
 /******************************************************/
 
 void lc_planner_init()
@@ -49,21 +51,42 @@ void lc_planner_plan()
 /*********************************************************/
 static void lc_planner_gcode_parser_callback(const lc_gcode_obj_t *parsed_gcode)
 {
+    static lc_stepper_t steps;
+
     switch (parsed_gcode->command_type)
     {
     case LC_GCODE_TYPE_G:
-    {
-        lc_stepper_t *steps = NULL;
-        // todo: ask motion control module to calculate steps and dir bit
-        lc_planner_rb_insert(rb, steps);
-    }
+        lc_planner_on_G_command(parsed_gcode);
     break;
     case LC_GCODE_TYPE_M:
+        lc_planner_on_M_command(parsed_gcode);
         break;
     case LC_GCODE_TYPE_F:
+        // todo: change the feedrate
         break;
     default:
         break;
     }
+}
+
+static void lc_planner_on_G_command(const lc_gcode_obj_t *parsed_gcode)
+{
+        lc_stepper_t *steps = NULL;
+
+        switch (parsed_gcode->command_number)
+        {
+        case 0: //G0
+        case 1: //G1
+            // todo: ask motion control module to calculate steps and dir bit
+            lc_planner_rb_insert(rb, steps);
+            break;
+        default:
+            break;
+        }
+}
+
+static void lc_planner_on_M_command(const lc_gcode_obj_t *parsed_gcode)
+{
+
 }
 /*********************************************************/
