def07a9b5b86fffae24acf74bd29cd64dd4564a6
radunasor <radmehr1385@gmail.com>
2025-01-19 13:20:08 +0100

    added forgotten argument in Mcode handler module

--- src/planner/m_commands.c
+++ src/planner/m_commands.c
@@ -10,8 +10,8 @@ static void lc_planner_m_command_m1(const lc_gcode_obj_t *parsed_code);
 struct m_commands_handlers_st
 {
     lc_planner_mcode_list_t code;
-    void (*handler_func)();
-}m_commands_handlers[] = {
+    void (*handler_func)(const lc_gcode_obj_t *);
+} m_commands_handlers[] = {
     {LC_PLANNER_MCODE_LIST_M0, lc_planner_m_command_m0},
     {LC_PLANNER_MCODE_LIST_M1, lc_planner_m_command_m1},
 
@@ -21,15 +21,15 @@ struct m_commands_handlers_st
 void lc_planner_on_M_command(const lc_gcode_obj_t *parsed_code)
 {
     uint8_t handler_index = 0;
-    while(m_commands_handlers[handler_index].code != _LC_PLANNER_MCODE_LIST_LAST)
+    while (m_commands_handlers[handler_index].code != _LC_PLANNER_MCODE_LIST_LAST)
     {
         if (m_commands_handlers[handler_index].code == (lc_planner_mcode_list_t)parsed_code->command_number)
-            {
-                if(m_commands_handlers[handler_index].handler_func)
-                    m_commands_handlers[handler_index].handler_func(parsed_code);
-                
-                break;
-            }
+        {
+            if (m_commands_handlers[handler_index].handler_func)
+                m_commands_handlers[handler_index].handler_func(parsed_code);
+
+            break;
+        }
         handler_index++;
     }
 }
@@ -39,11 +39,9 @@ void lc_planner_on_M_command(const lc_gcode_obj_t *parsed_code)
 /*********************************************************/
 static void lc_planner_m_command_m0(const lc_gcode_obj_t *parsed_code)
 {
-
 }
 
 static void lc_planner_m_command_m1(const lc_gcode_obj_t *parsed_code)
 {
-
 }
 /*********************************************************/
