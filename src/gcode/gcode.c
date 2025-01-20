8b51e74245f0a9124461a7196a9b8fc9a55b5064
radunasor <radmehr1385@gmail.com>
2025-01-20 16:31:36 +0100

    separated g/m code handeling parts

--- src/gcode/gcode.c
+++ src/gcode/gcode.c
@@ -2,34 +2,17 @@
 
 #include "gcode.h"
 #include "parser.h"
+#include "g_command.h"
 
 #define CHECK_INITIALIIZED LC_ASSERT(initialized, "GCODE module not initialized!");
 
 static bool initialized = false;
-static lc_gcode_parse_cb_t parse_cb = NULL;
 
-static lc_gcode_obj_t gcode_obj = {
+static lc_gcode_obj_t parsed_command = {
     .command_number = 0,
     .command_type = LC_GCODE_TYPE_NONE,
-    .command_values = NULL,
     .subcommand_existed = false,
     .sub_command_number = 0,
-};
-
-static lc_gcode_attrbute_value_t line_gcode_attributes =
-    {
-        .X = {false, 0},
-        .Y = {false, 0},
-        .Z = {false, 0},
-        .I = {false, 0},
-        .J = {false, 0},
-        .K = {false, 0},
-        .L = {false, 0},
-        .N = {false, 0},
-        .P = {false, 0},
-        .R = {false, 0},
-        .S = {false, 0},
-        .T = {false, 0},
 };
 
 /******************************************************/
@@ -51,22 +34,12 @@ void lc_gcode_deinit()
     if (!initialized)
         return;
 
-    memset(&line_gcode_attributes, 0, sizeof(lc_gcode_attrbute_value_t));
-    parse_cb = NULL;
     initialized = false;
 }
 
 bool lc_gcode_get_initialized()
 {
     return initialized;
-}
-
-void lc_gcode_set_parse_cb(lc_gcode_parse_cb_t parse_callback)
-{
-    if (!parse_callback)
-        return;
-
-    parse_cb = parse_callback;
 }
 
 bool lc_gcode_process_line(const char *line, size_t line_num)
@@ -90,34 +63,6 @@ static bool lc_gcode_parse_line(char *line, size_t line_num)
 {
     lc_gcode_parser_mark_comments(line);
 
-    struct lc_gcode_parse_tag_map
-    {
-        const char tag;
-        lc_gcode_attr_t *attr;
-    } lc_gcode_parser_tag_map[] = {
-        {'X', &line_gcode_attributes.X},
-        {'Y', &line_gcode_attributes.Y},
-        {'Z', &line_gcode_attributes.Z},
-        {'I', &line_gcode_attributes.I},
-        {'J', &line_gcode_attributes.J},
-        {'K', &line_gcode_attributes.K},
-        {'L', &line_gcode_attributes.L},
-        {'N', &line_gcode_attributes.N},
-        {'P', &line_gcode_attributes.P},
-        {'R', &line_gcode_attributes.R},
-        {'S', &line_gcode_attributes.S},
-        {'T', &line_gcode_attributes.T},
-
-        {'\0', NULL},
-    };
-
-    for (uint8_t i = 0; lc_gcode_parser_tag_map[i].tag; i++)
-        if (!lc_gcode_parser_get_value(line, lc_gcode_parser_tag_map[i].tag, &(lc_gcode_parser_tag_map[i].attr->existed), &(lc_gcode_parser_tag_map[i].attr->value)))
-        {
-            LC_LOG_ERROR("ERROR on GCODE Line %d", line_num);
-            return false;
-        }
-
     lc_gcode_command_type_t lc_gcode_command_type_arr[] = {
         LC_GCODE_TYPE_F,
         LC_GCODE_TYPE_M,
@@ -132,21 +77,35 @@ static bool lc_gcode_parse_line(char *line, size_t line_num)
     {
         bool command_existed = false;
         command_ptr = line;
+        parsed_command.command_type = lc_gcode_command_type_arr[i];
 
-        if (!lc_gcode_parser_get_command(&command_ptr, lc_gcode_command_type_arr[i], &command_existed, &gcode_obj.command_number, &gcode_obj.subcommand_existed, &gcode_obj.sub_command_number))
+        if (!lc_gcode_parser_get_command(&command_ptr, lc_gcode_command_type_arr[i], &command_existed, &parsed_command.command_number, &parsed_command.subcommand_existed, &parsed_command.sub_command_number))
             return false;
 
         while (command_existed)
         {
-            gcode_obj.command_type = lc_gcode_command_type_arr[i];
-            gcode_obj.command_values = line_gcode_attributes;
+            switch (lc_gcode_command_type_arr[i])
+            {
+            case LC_GCODE_TYPE_G:
+            {
+                lc_gcode_g_command_t g_command;
+                g_command.command = parsed_command;
+                lc_gcode_g_command_extract_values(&g_command.command, line);
+                break;
+            }
+            case LC_GCODE_TYPE_M:
+            {
+                lc_gcode_m_command_t m_command;
+                m_command.command = parsed_command;
+                lc_gcode_m_command_extract_attributes(&m_command.command, line);
+                break;
+            }
+            default:
+                break;
+            }
+            parsed_command.command_type = lc_gcode_command_type_arr[i];
 
-            if (parse_cb)
-                parse_cb(&gcode_obj);
-            else
-                LC_LOG_WARN("GCODE parsing callback has to be set to get the parsed result from the module");
-
-            if (!lc_gcode_parser_get_command(&command_ptr, lc_gcode_command_type_arr[i], &command_existed, &gcode_obj.command_number, &gcode_obj.subcommand_existed, &gcode_obj.sub_command_number))
+            if (!lc_gcode_parser_get_command(&command_ptr, lc_gcode_command_type_arr[i], &command_existed, &parsed_command.command_number, &parsed_command.subcommand_existed, &parsed_command.sub_command_number))
                 return false;
         }
     }
--- src/gcode/gcode.h
+++ src/gcode/gcode.h
@@ -2,59 +2,42 @@
 #define GCODE_H
 
 #include "common_defs.h"
-
-#define LC_GCODE_MAX_LINE_LENTGH 256
-
-typedef enum lc_gcode_command_type_st
-{
-    LC_GCODE_TYPE_NONE,
-    LC_GCODE_TYPE_G,
-    LC_GCODE_TYPE_M,
-    LC_GCODE_TYPE_F,
-
-    LC_GCODE_TYPE_LAST,
-} lc_gcode_command_type_t;
-
-typedef struct lc_gcode_attr_st
-{
-    bool existed;
-    float value;
-} lc_gcode_attr_t;
-
-typedef struct lc_gcode_attrbute_value_st
-{
-    lc_gcode_attr_t X;
-    lc_gcode_attr_t Y;
-    lc_gcode_attr_t Z;
-    lc_gcode_attr_t I;
-    lc_gcode_attr_t J;
-    lc_gcode_attr_t K;
-    lc_gcode_attr_t L;
-    lc_gcode_attr_t N;
-    lc_gcode_attr_t P;
-    lc_gcode_attr_t R;
-    lc_gcode_attr_t S;
-    lc_gcode_attr_t T;
-} lc_gcode_attrbute_value_t;
-
-typedef struct lc_gcode_obj_st
-{
-    lc_gcode_command_type_t command_type;
-    uint32_t command_number;
-    bool subcommand_existed;
-    uint16_t sub_command_number;
-    lc_gcode_attrbute_value_t command_values;
-}lc_gcode_obj_t;
-
-typedef void (*lc_gcode_parse_cb_t)(const lc_gcode_obj_t *gcode_obj);
+#include "gcode_base_obj.h"
+#include "g_command.h"
+#include "m_command.h"
 
 void lc_gcode_init();
 void lc_gcode_deinit();
 bool lc_gcode_get_initialized();
 
-void lc_gcode_set_parse_cb(lc_gcode_parse_cb_t parse_callback);
-bool lc_gcode_feed_line(char *line, size_t *line_number);
 bool lc_gcode_set_end_of_file();
 
 bool lc_gcode_process_line(const char *line, size_t line_num);
+
+lc_gcode_command_type_t inline lc_gcode_get_command_type(const lc_gcode_obj_t *gcode_obj){
+    LC_ASSERT(gcode_obj, "gcode obj can't be a NULL pointer!");
+    return gcode_obj->command_type;
+}
+
+uint32_t inline lc_gcode_get_command_number(const lc_gcode_obj_t *gcode_obj)
+{
+    LC_ASSERT(gcode_obj, "gcode obj can't be a NULL pointer!");
+
+    return gcode_obj->command_number;
+}
+
+bool inline lc_gcode_get_sub_command_existed(const lc_gcode_obj_t *gcode_obj)
+{
+    LC_ASSERT(gcode_obj, "gcode obj can't be a NULL pointer!");
+    
+    return gcode_obj->subcommand_existed;
+}
+
+uint16_t inline lc_gcode_get_sub_command_number(const lc_gcode_obj_t *gcode_obj)
+{
+    LC_ASSERT(gcode_obj, "gcode obj can't be a NULL pointer!");
+
+    return gcode_obj->sub_command_number;
+}
+
 #endif
